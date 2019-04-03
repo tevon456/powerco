239154f956a547dff705bd3b5dc6a1aef582d1c8
tevon <tevondavisjc@gmail.com>
2019-04-03 22:02:56 -0100

    made changes to tab text

--- PowerCo/MainPowerCo.Designer.cs
+++ PowerCo/MainPowerCo.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPowerCo));
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.BillGeneration = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabControl1.SuspendLayout();
+            this.CustomerManagement = new System.Windows.Forms.TabControl();
+            this.CustomerManagement.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabPage2
+            // BillGeneration
             // 
-            this.tabPage2.BackColor = System.Drawing.Color.RoyalBlue;
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(805, 431);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.BillGeneration.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BillGeneration.Location = new System.Drawing.Point(4, 22);
+            this.BillGeneration.Name = "BillGeneration";
+            this.BillGeneration.Padding = new System.Windows.Forms.Padding(3);
+            this.BillGeneration.Size = new System.Drawing.Size(805, 431);
+            this.BillGeneration.TabIndex = 1;
+            this.BillGeneration.Text = "Bill Generation";
             // 
             // tabPage1
             // 
@@ -53,21 +53,21 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(805, 431);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Customer Management";
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
-            // tabControl1
+            // CustomerManagement
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.CustomerManagement.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(-6, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(813, 457);
-            this.tabControl1.TabIndex = 1;
+            this.CustomerManagement.Controls.Add(this.tabPage1);
+            this.CustomerManagement.Controls.Add(this.BillGeneration);
+            this.CustomerManagement.Location = new System.Drawing.Point(-6, 0);
+            this.CustomerManagement.Name = "CustomerManagement";
+            this.CustomerManagement.SelectedIndex = 0;
+            this.CustomerManagement.Size = new System.Drawing.Size(813, 457);
+            this.CustomerManagement.TabIndex = 1;
             // 
             // MainPowerCo
             // 
@@ -75,20 +75,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.CustomerManagement);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainPowerCo";
             this.Text = "MainPowerCo";
             this.Load += new System.EventHandler(this.MainPowerCo_Load);
-            this.tabControl1.ResumeLayout(false);
+            this.CustomerManagement.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage BillGeneration;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl CustomerManagement;
     }
 }
